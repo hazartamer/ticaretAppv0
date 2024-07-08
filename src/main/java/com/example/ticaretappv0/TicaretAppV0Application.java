a044cb5b85233471a19f4971f0f5fcc353ffbf96
hazartamer <hazartamer23@gmail.com>
2024-07-08 13:07:12 +0300

    first commit

---
+++ src/main/java/com/example/ticaretappv0/TicaretAppV0Application.java
@@ -0,0 +1,13 @@
+package com.example.ticaretappv0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicaretAppV0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicaretAppV0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ticaretappv0/TicaretAppV0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticaretappv0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicaretAppV0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
